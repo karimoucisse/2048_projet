c0b2ffad7fe9de0c9ec946bfe1f88cbb62c74b7f
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-11-08 17:06:34 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ class App extends Component {
     return (
       <div>
         <div>
-          <p>Welcome to the newest game of the year!</p>
-          <p>Who will have the best score ever?!</p>
           <h1>2048</h1>
         </div>
       </div>
