92dd3997203c0354244d95c75abf5683005d6131
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-11-08 16:53:00 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        <div>
+          <p>Welcome to the newest game of the year!</p>
+          <p>Who will have the best score ever?!</p>
+          <h1>2048</h1>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
